4058fea203fe6e15a28707cfe1c1436836498960
MahderK <mahderkassaw@gmail.com>
2026-03-10 21:47:54 +0300

    feat: added testimonial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar"
 import Hero from "@/components/Hero"
 import Footer from "@/components/Footer"
 import Feature from "@/components/Feature"
+import TestimonialSection from "@/components/Testimonial"
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <Hero />
             <main>
                 <Feature />
+                <TestimonialSection />
             </main>
             <footer>
                 <Footer />
